5f6b85e6b43908bc62560de13a3139d48520f371
Kashif khan <159754234+Kashif09876@users.noreply.github.com>
2024-04-05 22:12:43 +0500

    Add files via upload
    Cli-Number-Guessing

---
+++ game.ts
@@ -0,0 +1,17 @@
+#! /user/bin/env node
+
+import inquirer from "inquirer";
+
+let randomNumber = Math.floor(Math.random() *6)+1;
+
+const answers = await inquirer.prompt({
+    name:"randomNumber",
+    type:"number",
+    message:"Guess The Random Number:",
+});
+
+if(answers.randomNumber === randomNumber){
+    console.log("Congratulations Your Guess Number Is Correct")
+}else{
+    console.log("Your Guess Number Is Not Correct Please Try Again Later Thank You!") 
+}
